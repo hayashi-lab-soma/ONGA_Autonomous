96e63700f0c8de9f2dff8b8d9d5a4139be8046e5
takumitomokawa <tomokawa.takumi163@mail.kyutech.jp>
2021-07-07 22:50:10 +0900

    fix by tomokawa

--- onga_perception/scripts/detect_centroid.py
+++ onga_perception/scripts/detect_centroid.py
@@ -12,50 +12,48 @@ from sensor_msgs.msg import Image
 
 class DetectCentroid(object):
     def __init__(self):
+        rospy.loginfo("initialize DetectionCentroid")
         # self.sub_rgb = message_filters.Subscriber("/camera/color/image_raw",Image)
         # self.sub_depth = message_filters.Subscriber("/camera/aligned_depth_to_color/image_raw",Image)
         self.bridge = CvBridge()
 
     def RGBCallback(self, rgb_data):
+        rospy.loginfo("success sub")
         color_image = self.bridge.imgmsg_to_cv2(rgb_data)
         color_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2BGR)
         
-        while True:
-            bgrLower = np.array([30, 80, 150])    
-            bgrUpper = np.array([120, 150, 255])  
-            mask_image = cv2.inRange(color_image, bgrLower, bgrUpper)
-            # result = cv2.bitwise_and(self.color_image, self.color_image, mask=img_mask)
+        bgrLower = np.array([30, 80, 150])    
+        bgrUpper = np.array([120, 150, 255])  
+        mask_image = cv2.inRange(color_image, bgrLower, bgrUpper)
+        result = cv2.bitwise_and(color_image, color_image, mask=mask_image)
+        
+        M = cv2.moments(mask_image)
+        if M["m00"] == 0 or M["m00"] == 0 :
+            cX = -1
+            cY = -1       
+        else :
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            cv2.circle(color_image, (cX, cY), 5, (0, 0, 0), -1)
+
+        # contours = cv2.findContours(mask_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+        
+        # area_thresh = 10000
+        # contours = list(filter(lambda x: cv2.contourArea(x) > area_thresh, contours))
+
+        
+        # for cnt in contours:
+        
+        #     x, y, width, height = cv2.boundingRect(cnt)
             
-            M = cv2.moments(mask_image)
-            if M["m00"] == 0 or M["m00"] == 0 :
-                cX = -1
-                cY = -1       
-            else :
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                cv2.circle(color_image, (cX, cY), 5, (0, 0, 0), -1)
+        #     cv2.rectangle(color_image, (x, y), (x + width, y + height), color=(0, 255, 0), thickness=2)
 
-            contours = cv2.findContours(mask_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
-            
-            area_thresh = 10000
-            contours = list(filter(lambda x: cv2.contourArea(x) > area_thresh, contours))
+        # cv2.imshow("image", color_image)
+        # # cv2.imshow("mask", result)
+        # cv2.imwrite("ret.png", color_image)
+        # cv2.waitKey(1)
 
-            
-            for cnt in contours:
-            
-                x, y, width, height = cv2.boundingRect(cnt)
-                
-                cv2.rectangle(color_image, (x, y), (x + width, y + height), color=(0, 255, 0), thickness=2)
-
-            cv2.imshow("image", color_image)
-            # cv2.imshow("mask", result)
-            cv2.imwrite("ret.png", color_image)
-            cv2.waitKey(1)
-
-            # rospy.loginfo("centroid", cX, cY)
-
-    # def DepthCallback(self, depth_data):
-    #     self.depth_image = self.bridge.imgmsg_to_cv2(depth_data).astype(np.uint16)
+        # rospy.loginfo("centroid", cX, cY)
 
     def Process(self):
         rospy.init_node("detect_centroid", anonymous=True)
