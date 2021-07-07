5bc0ef9a5ed32e816b9459b1b3d4a45aea3d0bdd
yamasaki <yamasaki.haruna929@mail.kyutech.jp>
2021-07-07 22:26:07 +0900

    fix detect_centroid

--- onga_perception/scripts/detect_centroid.py
+++ onga_perception/scripts/detect_centroid.py
@@ -1,71 +1,74 @@
-import pyrealsense2 as rs
+#!/usr/bin/env python
 import cv2
 import numpy as np
 import math
+from rosgraph.names import anonymous_name
 import rospy
 import rosparam
 import sys
 import message_filters
 from cv_bridge import CvBridge, CvBridgeError
-from std_msgs.msg import Image
+from sensor_msgs.msg import Image
 
-class depth_estimater:
-    WIDTH = 12
-    HEIGHT = 6
-  
+class DetectCentroid(object):
     def __init__(self):
-  
-        rospy.init_node('depth_estimater', anonymous=True)
+        # self.sub_rgb = message_filters.Subscriber("/camera/color/image_raw",Image)
+        # self.sub_depth = message_filters.Subscriber("/camera/aligned_depth_to_color/image_raw",Image)
         self.bridge = CvBridge()
-        sub_rgb = message_filters.Subscriber("/camera/color/image_raw",Image)
-        sub_depth = message_filters.Subscriber("/camera/depth/image_rect_raw",Image)
-        self.mf = message_filters.ApproximateTimeSynchronizer([sub_rgb, sub_depth], 100, 0.5)
-        self.mf.registerCallback(self.ImageCallback)
-  
-    def ImageCallback(self, rgb_data , depth_data):
-        try:
-            color_image = self.bridge.imgmsg_to_cv2(rgb_data, 'passthrough')
-            color_image = cv2.resize(color_image, (int(color_image.shape[1] * 0.1),\ 
-                (int(color_image.shape[0] * 0.1)))
-            depth_image = self.bridge.imgmsg_to_cv2(depth_data, 'passthrough')
-            depth_image = cv2.resize(depth_image, (int(depth_image.shape[1] * 0.1),\ 
-                (int(depth_image.shape[0] * 0.1)))
-        except CvBridgeError, e:
-            rospy.logerr(e)
-  
-        color_image.flags.writeable = True
-        #color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB) 
-        #color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
-        h, w, c = color_image.shape
-  
-        x1 = (w / 2) - self.WIDTH
-        x2 = (w / 2) + self.WIDTH
-        y1 = (h / 2) - self.HEIGHT
-        y2 = (h / 2) + self.HEIGHT
-        sum = 0.0
-  
-        for i in range(y1, y2):
-            for j in range(x1, x2):
-                color_image.itemset((i, j, 0), 0)
-                color_image.itemset((i, j, 1), 0)
-  
-                if depth_image.item(i,j) == depth_image.item(i,j):
-                    sum += depth_image.item(i,j)
-  
-        ave = sum / ((self.WIDTH * 2) * (self.HEIGHT * 2))
-        print("%f [m]" % ave)
-  
-        cv2.normalize(depth_image, depth_image, 0, 1, cv2.NORM_MINMAX)
-        cv2.namedWindow("color_image")
-        cv2.namedWindow("depth_image")
-        cv2.imshow("color_image", color_image)
-        cv2.imshow("depth_image", depth_image)
-        cv2.waitKey(100)
+
+    def RGBCallback(self, rgb_data):
+        color_image = self.bridge.imgmsg_to_cv2(rgb_data)
+        color_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2BGR)
+        
+        while True:
+            bgrLower = np.array([30, 80, 150])    
+            bgrUpper = np.array([120, 150, 255])  
+            mask_image = cv2.inRange(color_image, bgrLower, bgrUpper)
+            # result = cv2.bitwise_and(self.color_image, self.color_image, mask=img_mask)
+            
+            M = cv2.moments(mask_image)
+            if M["m00"] == 0 or M["m00"] == 0 :
+                cX = -1
+                cY = -1       
+            else :
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                cv2.circle(color_image, (cX, cY), 5, (0, 0, 0), -1)
+
+            contours = cv2.findContours(mask_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+            
+            area_thresh = 10000
+            contours = list(filter(lambda x: cv2.contourArea(x) > area_thresh, contours))
+
+            
+            for cnt in contours:
+            
+                x, y, width, height = cv2.boundingRect(cnt)
+                
+                cv2.rectangle(color_image, (x, y), (x + width, y + height), color=(0, 255, 0), thickness=2)
+
+            cv2.imshow("image", color_image)
+            # cv2.imshow("mask", result)
+            cv2.imwrite("ret.png", color_image)
+            cv2.waitKey(1)
+
+            # rospy.loginfo("centroid", cX, cY)
+
+    # def DepthCallback(self, depth_data):
+    #     self.depth_image = self.bridge.imgmsg_to_cv2(depth_data).astype(np.uint16)
+
+    def Process(self):
+        rospy.init_node("detect_centroid", anonymous=True)
+        rospy.Subscriber("/camera/color/image_raw", Image, self.RGBCallback)
+        rospy.spin()
+        # rospy.Subscriber(self.DEPTH['topic'], self.DEPTH['depth_data'], self.DepthCallback)
+            
   
 if __name__ == '__main__':
     try:
-        de = depth_estimater()
-        rospy.spin()
+        detect_centroid = DetectCentroid()
+        detect_centroid.Process()
+
     except rospy.ROSInterruptException: pass
 
 
