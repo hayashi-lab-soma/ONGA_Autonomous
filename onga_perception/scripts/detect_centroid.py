974849cbbfb33ae812ed891e3c3939b92d88139e
yamasaki <yamasaki.haruna929@mail.kyutech.jp>
2021-07-07 18:06:25 +0900

    new

--- onga_perception/scripts/detect_centroid.py
+++ onga_perception/scripts/detect_centroid.py
@@ -2,63 +2,70 @@ import pyrealsense2 as rs
 import cv2
 import numpy as np
 import math
+import rospy
+import rosparam
+import sys
+import message_filters
+from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import Image
 
-def  cameraSetting(width=1280, height=720):
-    # Get device product line for setting a supporting resolution
-    pipeline = rs.pipeline()
-    config = rs.config() # Configure streams
-
-    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-    pipeline_profile = config.resolve(pipeline_wrapper)
-    device = pipeline_profile.get_device()
-    # device_product_line = str(device.get_info(rs.camera_info.product_line))
-
-    config.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
-
-    cfg = pipeline.start(config)
-
-    # Camera calibration data
-    profile = cfg.get_stream(rs.stream.depth) # Fetch stream profile for depth stream
-    intr = profile.as_video_stream_profile().get_intrinsics() # Downcast to video_stream_profile and fetch intrinsics
-
-    camera_matrix = np.array([[intr.fx, 0, intr.ppx], [0, intr.fy, intr.ppy], [0, 0, 1]], dtype=np.float32) 
-    camera_distortion = np.array([0,0,0,0,0], dtype=np.float32)
-
-    return pipeline, camera_matrix, camera_distortion,intr
-
-def cameraProcess(pipeline):
-    align_to = rs.stream.color
-    align = rs.align(align_to)
-
-    frames = pipeline.wait_for_frames()
-        
-    # Align the depth frame to color frame
-    aligned_frames = align.process(frames)
-    
-    depth_frame = aligned_frames.get_depth_frame()
-    color_frame = aligned_frames.get_color_frame()
-    if not depth_frame or not color_frame: pass
-
-    hole_filling = rs.hole_filling_filter()
-    filled_frame = hole_filling.process(depth_frame)
-    
-    # Convert images to numpy arrays
-    filled_depth_image = np.asanyarray(filled_frame.get_data())
-    color_image = np.asanyarray(color_frame.get_data())
-
-    return filled_depth_image, color_image,filled_frame
-
-def main():
-    width=640 ; height=480
-    pipeline, camera_matrix, camera_distortion,intr = cameraSetting(width, height)
-
-    while True:
-        depth_image, color_image,depth_frame = cameraProcess(pipeline)
-        cv2.imshow("image", color_image)
-        cv2.waitKey(1)
-
+class depth_estimater:
+    WIDTH = 12
+    HEIGHT = 6
+  
+    def __init__(self):
+  
+        rospy.init_node('depth_estimater', anonymous=True)
+        self.bridge = CvBridge()
+        sub_rgb = message_filters.Subscriber("/camera/color/image_raw",Image)
+        sub_depth = message_filters.Subscriber("/camera/depth/image_rect_raw",Image)
+        self.mf = message_filters.ApproximateTimeSynchronizer([sub_rgb, sub_depth], 100, 0.5)
+        self.mf.registerCallback(self.ImageCallback)
+  
+    def ImageCallback(self, rgb_data , depth_data):
+        try:
+            color_image = self.bridge.imgmsg_to_cv2(rgb_data, 'passthrough')
+            color_image = cv2.resize(color_image, (int(color_image.shape[1] * 0.1),\ 
+                (int(color_image.shape[0] * 0.1)))
+            depth_image = self.bridge.imgmsg_to_cv2(depth_data, 'passthrough')
+            depth_image = cv2.resize(depth_image, (int(depth_image.shape[1] * 0.1),\ 
+                (int(depth_image.shape[0] * 0.1)))
+        except CvBridgeError, e:
+            rospy.logerr(e)
+  
+        color_image.flags.writeable = True
+        #color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB) 
+        #color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+        h, w, c = color_image.shape
+  
+        x1 = (w / 2) - self.WIDTH
+        x2 = (w / 2) + self.WIDTH
+        y1 = (h / 2) - self.HEIGHT
+        y2 = (h / 2) + self.HEIGHT
+        sum = 0.0
+  
+        for i in range(y1, y2):
+            for j in range(x1, x2):
+                color_image.itemset((i, j, 0), 0)
+                color_image.itemset((i, j, 1), 0)
+  
+                if depth_image.item(i,j) == depth_image.item(i,j):
+                    sum += depth_image.item(i,j)
+  
+        ave = sum / ((self.WIDTH * 2) * (self.HEIGHT * 2))
+        print("%f [m]" % ave)
+  
+        cv2.normalize(depth_image, depth_image, 0, 1, cv2.NORM_MINMAX)
+        cv2.namedWindow("color_image")
+        cv2.namedWindow("depth_image")
+        cv2.imshow("color_image", color_image)
+        cv2.imshow("depth_image", depth_image)
+        cv2.waitKey(100)
+  
 if __name__ == '__main__':
-    main()
-    
-    
+    try:
+        de = depth_estimater()
+        rospy.spin()
+    except rospy.ROSInterruptException: pass
+
+
