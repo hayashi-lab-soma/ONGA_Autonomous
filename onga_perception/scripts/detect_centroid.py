f4f2502e24751235f88fa7868e2db42312773de7
Tsuji <tsuji1998t10@gmail.com>
2021-07-08 21:02:55 +0900

    change to ros_numpy from bridge

--- onga_perception/scripts/detect_centroid.py
+++ onga_perception/scripts/detect_centroid.py
@@ -9,19 +9,22 @@ import sys
 import message_filters
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+import ros_numpy as rosnp
 
 class DetectCentroid(object):
     def __init__(self):
         rospy.loginfo("initialize DetectionCentroid")
         # self.sub_rgb = message_filters.Subscriber("/camera/color/image_raw",Image)
         # self.sub_depth = message_filters.Subscriber("/camera/aligned_depth_to_color/image_raw",Image)
-        self.bridge = CvBridge()
+        # self.bridge = CvBridge()
 
     def RGBCallback(self, rgb_data):
         rospy.loginfo("success sub")
-        color_image = self.bridge.imgmsg_to_cv2(rgb_data)
+        # color_image = self.bridge.imgmsg_to_cv2(rgb_data)
+        # color_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2BGR)
+        color_image = rosnp.numpify(rgb_data)
         color_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2BGR)
-        
+
         bgrLower = np.array([30, 80, 150])    
         bgrUpper = np.array([120, 150, 255])  
         mask_image = cv2.inRange(color_image, bgrLower, bgrUpper)
@@ -36,24 +39,24 @@ class DetectCentroid(object):
             cY = int(M["m01"] / M["m00"])
             cv2.circle(color_image, (cX, cY), 5, (0, 0, 0), -1)
 
-        # contours = cv2.findContours(mask_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+        contours = cv2.findContours(mask_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
         
-        # area_thresh = 10000
-        # contours = list(filter(lambda x: cv2.contourArea(x) > area_thresh, contours))
+        area_thresh = 10000
+        contours = list(filter(lambda x: cv2.contourArea(x) > area_thresh, contours))
 
         
-        # for cnt in contours:
+        for cnt in contours:
         
-        #     x, y, width, height = cv2.boundingRect(cnt)
+            x, y, width, height = cv2.boundingRect(cnt)
             
-        #     cv2.rectangle(color_image, (x, y), (x + width, y + height), color=(0, 255, 0), thickness=2)
+            cv2.rectangle(color_image, (x, y), (x + width, y + height), color=(0, 255, 0), thickness=2)
 
-        # cv2.imshow("image", color_image)
-        # # cv2.imshow("mask", result)
+        cv2.imshow("image", color_image)
+        cv2.imshow("mask", result)
         # cv2.imwrite("ret.png", color_image)
-        # cv2.waitKey(1)
+        cv2.waitKey(1)
 
-        # rospy.loginfo("centroid", cX, cY)
+        rospy.loginfo("centroid", cX, cY)
 
     def Process(self):
         rospy.init_node("detect_centroid", anonymous=True)
