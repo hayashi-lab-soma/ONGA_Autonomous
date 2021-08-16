965ff61e42f4973c3d5dc8ff46a1bda3ed37ceba
Tsuji <tsuji1998t10@gmail.com>
2021-08-16 12:13:10 +0900

    successful for real demo

--- onga_perception/scripts/get_redcarpet_planenormal.py
+++ onga_perception/scripts/get_redcarpet_planenormal.py
@@ -20,28 +20,28 @@ import open3d as o3d
 
 def send_traj_point_marker(marker_pub, pose):
     marker = Marker()
-    # marker.header.frame_id = "/camera_color_optical_frame"
-    marker.header.frame_id = "/base_link"
+    marker.header.frame_id = "/camera_color_optical_frame"
+    # marker.header.frame_id = "/camera_realsense_gazebo"
     marker.header.stamp = rospy.Time.now()
     marker.ns = "traj_point" 
     marker.id = 0
     marker.type = Marker.SPHERE
     marker.action = Marker.ADD
     marker.pose = pose
-    marker.scale.x = 0.2
-    marker.scale.y = 0.2
-    marker.scale.z = 0.2
-    marker.color.r = 1.0
-    marker.color.g = 0.0
-    marker.color.b = 0.0
+    marker.scale.x = 0.02 #0.2(gazebo)
+    marker.scale.y = 0.02
+    marker.scale.z = 0.02
+    marker.color.r = 0.04
+    marker.color.g = 0.68
+    marker.color.b = 0.5
     marker.color.a = 1.0
     marker.lifetime = rospy.Duration(0.3,0)
     marker_pub.publish(marker)
 
 def send_traj_line_marker(marker_pub, pose, points):
     marker = Marker()
-    # marker.header.frame_id = "/camera_color_optical_frame"
-    marker.header.frame_id = "/base_link"
+    marker.header.frame_id = "/camera_color_optical_frame"
+    # marker.header.frame_id = "/camera_realsense_gazebo"
     marker.header.stamp = rospy.Time.now()
     marker.ns = "traj_point" 
     marker.id = 0
@@ -49,12 +49,12 @@ def send_traj_line_marker(marker_pub, pose, points):
     marker.action = Marker.ADD
     marker.pose = pose
     marker.points = points
-    marker.scale.x = 0.2
-    marker.scale.y = 0.2
-    marker.scale.z = 0.2
-    marker.color.r = 1.0
-    marker.color.g = 0.0
-    marker.color.b = 0.0
+    marker.scale.x = 0.02
+    marker.scale.y = 0.02
+    marker.scale.z = 0.02
+    marker.color.r = 1.1
+    marker.color.g = 1.1
+    marker.color.b = 1.1
     marker.color.a = 1.0
     marker.lifetime = rospy.Duration(0.3,0)
     marker_pub.publish(marker)
@@ -64,16 +64,16 @@ class DetectCentroid(object):
         rospy.loginfo("initialize DetectionCentroid")
 
         ## For real device
-        # self.CAMINFO = {'topic': '/camera/color/camera_info', 'msg': CameraInfo}
-        # self.COLOR = {'topic': '/camera/color/image_raw', 'msg': Image}
-        # self.DEPTH = {'topic': '/camera/aligned_depth_to_color/image_raw', 'msg': Image}
+        self.CAMINFO = {'topic': '/camera/color/camera_info', 'msg': CameraInfo}
+        self.COLOR = {'topic': '/camera/color/image_raw', 'msg': Image}
+        self.DEPTH = {'topic': '/camera/aligned_depth_to_color/image_raw', 'msg': Image}
         
         ## For simulation
-        self.CAMINFO = {'topic': '/realsense/color/camera_info', 'msg': CameraInfo}
-        self.COLOR = {'topic': '/realsense/color/image_raw', 'msg': Image}
-        self.DEPTH = {'topic': '/realsense/depth/image_rect_raw', 'msg': Image}
-        self.H = 480
-        self.W = 640
+        # self.CAMINFO = {'topic': '/realsense/color/camera_info', 'msg': CameraInfo}
+        # self.COLOR = {'topic': '/realsense/color/image_raw', 'msg': Image}
+        # self.DEPTH = {'topic': '/realsense/depth/image_rect_raw', 'msg': Image}
+        self.H = 720#480
+        self.W = 1280#640
 
         self.header = Header()
         self.fields = [PointField('x', 0, 7, 1), PointField('y', 4, 7, 1), PointField('z', 8, 7, 1), PointField('rgb', 16, 7, 1)]
@@ -127,14 +127,13 @@ class DetectCentroid(object):
 
     def depthToPointcloud(self, depth_image):
         [height, width] = depth_image.shape
-        print(f'depthshape{depth_image.shape}')
         nx = np.linspace(0, width-1, width)
         ny = np.linspace(0, height-1, height)
         u, v = np.meshgrid(nx, ny)
         x = (u.flatten() - self.K[2])/self.K[0]
         y = (v.flatten() - self.K[5])/self.K[4]
 
-        z = depth_image.flatten()#/ 1000.0
+        z = depth_image.flatten() * self.depth_scale #/ 1000.0
         x = np.multiply(x,z)
         y = np.multiply(y,z)
 
@@ -142,19 +141,16 @@ class DetectCentroid(object):
         y = y[np.nonzero(z)]
         z = z[np.nonzero(z)]
         
-        # y     = -x#*1000
-        # z     = abs(y)#*1000
-        # x     = z#*1000
-
-        points = np.stack((z, -x, y), axis = -1)
+        points = np.stack((x, y, z), axis = -1)
 
         return points
 
     def publishPointcloud(self, pc_pub, points, color):
         points_color = self.addColorToPoints(points, color)
         header = self.header
-        # print(header)
-        header.frame_id = "/base_link"
+        # header.frame_id = "/camera_realsense_gazebo"
+        header.frame_id = "/camera_color_optical_frame"
+        header.stamp = rospy.Time.now()
         pc = point_cloud2.create_cloud(header, self.fields, points_color)
         pc_pub.publish(pc)
 
@@ -174,18 +170,41 @@ class DetectCentroid(object):
     def window(self):
         cv2.namedWindow("Range HSV")
         cv2.resizeWindow("Range HSV", 500, 350)
+        ######################gray_object######################
         # cv2.createTrackbar("HUE Min", "Range HSV", 2,180, self.empty)#105,180 orange corn
         # cv2.createTrackbar("HUE Max", "Range HSV", 180,180, self.empty)#168,180
         # cv2.createTrackbar("SAT Min", "Range HSV", 0,255, self.empty)#22,255
         # cv2.createTrackbar("SAT Max", "Range HSV", 235,255, self.empty)#149,255
         # cv2.createTrackbar("VALUE Min", "Range HSV",13,255, self.empty)#0,255
         # cv2.createTrackbar("VALUE Max", "Range HSV", 147,255, self.empty)#50,255
-        cv2.createTrackbar("HUE Min", "Range HSV", 22,180, self.empty)#105,180 orange corn
+        ######################gray_object######################
+
+        # ######################yellow_landmark######################
+        # cv2.createTrackbar("HUE Min", "Range HSV", 22,180, self.empty)#105,180 orange corn
+        # cv2.createTrackbar("HUE Max", "Range HSV", 180,180, self.empty)#168,180
+        # cv2.createTrackbar("SAT Min", "Range HSV", 24,255, self.empty)#22,255
+        # cv2.createTrackbar("SAT Max", "Range HSV", 237,255, self.empty)#149,255
+        # cv2.createTrackbar("VALUE Min", "Range HSV",0,255, self.empty)#0,255
+        # cv2.createTrackbar("VALUE Max", "Range HSV", 19,255, self.empty)#50,255
+        # ######################yellow_landmark######################
+
+        # ######################green_jetson_case######################
+        # cv2.createTrackbar("HUE Min", "Range HSV", 64,180, self.empty)#105,180 orange corn
+        # cv2.createTrackbar("HUE Max", "Range HSV", 180,180, self.empty)#168,180
+        # cv2.createTrackbar("SAT Min", "Range HSV", 182,255, self.empty)#22,255
+        # cv2.createTrackbar("SAT Max", "Range HSV", 255,255, self.empty)#149,255
+        # cv2.createTrackbar("VALUE Min", "Range HSV",96,255, self.empty)#0,255
+        # cv2.createTrackbar("VALUE Max", "Range HSV", 186,255, self.empty)#50,255
+        # ######################green_jetson_case######################
+
+        ######################under_graduate_cirtification######################
+        cv2.createTrackbar("HUE Min", "Range HSV", 15,180, self.empty)#105,180 orange corn
         cv2.createTrackbar("HUE Max", "Range HSV", 180,180, self.empty)#168,180
-        cv2.createTrackbar("SAT Min", "Range HSV", 24,255, self.empty)#22,255
-        cv2.createTrackbar("SAT Max", "Range HSV", 237,255, self.empty)#149,255
+        cv2.createTrackbar("SAT Min", "Range HSV", 31,255, self.empty)#22,255
+        cv2.createTrackbar("SAT Max", "Range HSV", 138,255, self.empty)#149,255
         cv2.createTrackbar("VALUE Min", "Range HSV",0,255, self.empty)#0,255
         cv2.createTrackbar("VALUE Max", "Range HSV", 19,255, self.empty)#50,255
+        ######################under_graduate_cirtification######################
 
     def get_hsvcentroid(self):
         h_min = cv2.getTrackbarPos("HUE Min", "Range HSV")
@@ -204,11 +223,12 @@ class DetectCentroid(object):
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,6))
         mask_image = cv2.erode(mask_image, kernel, iterations = 1)
         mask_image = cv2.dilate(mask_image, kernel, iterations = 1)
-        depth_image = np.asanyarray(self.depth_image)
+        depth_image = self.depth_image#np.asanyarray(self.depth_image)
         result = cv2.bitwise_and(color_image, color_image, mask=mask_image)
 
         cv2.imshow("RGB", color_image)
-        cv2.imshow("MASK", result)
+        cv2.imshow("MASKbw", result)
+        cv2.imshow("MASK", mask_image)
         cv2.waitKey(1)
         
         contours, hierarchy = cv2.findContours(mask_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -227,10 +247,26 @@ class DetectCentroid(object):
                 # cv2.waitKey(1)
 
                 ##--- Marker
+                ######################base_link######################
+                # new_pose = Pose()
+                # new_pose.position.y     = -depth_point[0]*1000  
+                # new_pose.position.z     = abs(depth_point[1]*1000)
+                # new_pose.position.x     = depth_point[2]*1000
+                ######################base_link######################
+
+                ######################camera_realsense_gazebo######################
                 new_pose = Pose()
-                new_pose.position.y     = -depth_point[0]*1000  
-                new_pose.position.z     = abs(depth_point[1]*1000)
-                new_pose.position.x     = depth_point[2]*1000
+                new_pose.position.x     = depth_point[0]*1000  
+                new_pose.position.y     = depth_point[1]*1000
+                new_pose.position.z     = depth_point[2]*1000
+                ######################camera_realsense_gazebo######################
+
+                ######################camera_color_optical_frame######################
+                new_pose = Pose()
+                new_pose.position.x     = depth_point[0]  
+                new_pose.position.y     = depth_point[1]
+                new_pose.position.z     = depth_point[2]
+                ######################camera_color_optical_frame######################
 
                 new_pose.orientation.x  = 0.0
                 new_pose.orientation.y  = 0.0
@@ -246,8 +282,7 @@ class DetectCentroid(object):
                 # mask_int = image_binary.astype(np.uint16)*255
                 ret, thresh = cv2.threshold(mask_int, 127, 255, 0)
                 masked_depth[np.nonzero(thresh)] = depth_image[np.nonzero(thresh)]
-                points = self.depthToPointcloud(masked_depth)
-                self.header.stamp = rospy.Time.now()
+                points = self.depthToPointcloud(masked_depth) #* 1000
                 self.publishPointcloud(self.pc_pub_karaage, points, self.rgb_red)
 
                 pcd = o3d.geometry.PointCloud()
@@ -255,17 +290,19 @@ class DetectCentroid(object):
                 downpcd = pcd.voxel_down_sample(voxel_size=0.05)
                 # pcd = o3d.io.read_point_cloud(points)
                 plane_model, inliers = downpcd.segment_plane(distance_threshold=5.0,
-                                                        ransac_n=50,
+                                                        ransac_n=5,#gazebo : 50
                                                         num_iterations=1000)
                 [a, b, c, d] = plane_model
                 # o3d.visualization.draw_geometries([downpcd])
 
                 print(f"Plane equation: {a:.5f}x + {b:.5f}y + {c:.5f}z + {d:.5f} = 0")
 
-                y1 = -10
-                y2 = 10
-                x1 = -c*y1/a
-                x2 = -c*y2/a
+                z1 = depth_point[2]#*1000
+                z2 = 0
+                x1 = depth_point[0]#*1000#-c*y1/a
+                # x2 = a*z2/b
+                x2 = -(b*(z1-z2)/a+x1)
+                print(f'x2{x2}')
                 ##--- LINE Marker
                 line_pose = Pose()
                 line_pose.position.y     = 0.0
@@ -281,14 +318,14 @@ class DetectCentroid(object):
                 line = []
                 # first point
                 first_line_point = Point()
-                first_line_point.y = y1
-                first_line_point.z = abs(depth_point[1]*1000)
+                first_line_point.y = 0
+                first_line_point.z = z1#abs(depth_point[1]*1000)
                 first_line_point.x = x1
                 line.append(first_line_point)
                 # second point
                 second_line_point = Point()
-                second_line_point.y = y2
-                second_line_point.z = abs(depth_point[1]*1000)
+                second_line_point.y = 0
+                second_line_point.z = z2#abs(depth_point[1]*1000)
                 second_line_point.x = x2
                 line.append(second_line_point)
                 ##--- END : LINE Marker
