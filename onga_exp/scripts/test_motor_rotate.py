cc7aa6e685d298e80d275bd493ac27375b693428
takumitomokawa <tomokawa.takumi163@mail.kyutech.jp>
2021-08-26 22:35:10 +0900

    update test_motor

--- onga_exp/scripts/test_motor_rotate.py
+++ onga_exp/scripts/test_motor_rotate.py
@@ -1,28 +1,56 @@
-import os
-import time
+#!/usr/bin/env python
 import rospy
-from threading import Thread
-from geometry_msgs.msg import Quaternion, Twist
-from math import pi
+import time
+from geometry_msgs.msg import Twist
 
-cond = True
+class Test():
+    def __init__(self):
+        self.pub = rospy.Publisher('/onga_velocity_controller/cmd_vel', Twist, queue_size=1)
+        self.stop = Twist()
+        self.stop.linear.x = 0
+        self.stop.angular.z = 0
 
-def cond_switch():
-    global cond
-    time.sleep(1)
-    cond = False
+    # go straight
+    def pub_linear(self):
+        dist = 3.0 # [m]
+        speed = 0.3 # [m/s]
+        target_time = dist / speed # [s]
+
+        t = Twist()
+        t.linear.x = speed
+        t.angular.z = 0            
+        start_time = time.time()
+        end_time = time.time()
+
+        rate = rospy.Rate(30)
+        while end_time - start_time <= target_time:
+            self.pub.publish(t)
+            end_time = time.time()
+            rate.sleep()
+        self.pub.publish(self.stop)
+
+    #rotate
+    def pub_angular(self):
+        theta = 180.0 # [deg]
+        speed = 60.0 # [deg/s]
+        target_time = theta / speed # [s]
+
+        t = Twist()
+        t.linear.x = 0
+        t.angular.z = speed * 3.1415 / 180.0 # [rad]
+        start_time = time.time()
+        end_time = time.time()
+
+        rate = rospy.Rate(30)
+        while end_time - start_time <= target_time:
+            self.pub.publish(t)
+            end_time = time.time()
+            rate.sleep()
+        self.pub.publish(self.stop)
+
 
 if __name__ == '__main__':
-    try:
-        rospy.init_node('wp_navi', anonymous=True)
-        thread = Thread(target=cond_switch)
-        thread.start()
-        while cond:
-            
-
-        
-
-
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        rospy.loginfo("WP navigation finished.")
+    rospy.init_node('tcmdvel_publisher')
+    test = Test()
+    # test.pub_linear()
+    test.pub_angular()
