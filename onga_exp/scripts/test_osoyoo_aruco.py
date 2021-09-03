cdfe569c861345cdb018fa195d34d80501d06d59
tomokawa <tomokawa.takumi163@mail.kyutech.jp>
2021-09-03 23:40:54 +0900

    add aruco transform

--- onga_exp/scripts/test_osoyoo_aruco.py
+++ onga_exp/scripts/test_osoyoo_aruco.py
@@ -5,14 +5,17 @@ import rospy
 import tf
 import actionlib
 from actionlib_msgs.msg import *
-from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point, Quaternion, Twist
+from geometry_msgs.msg import Quaternion, PoseStamped
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from math import pi
 from std_msgs.msg import Bool,Float32MultiArray
+from math import radians
 
-class WpNavi():
+class ArucoNavi():
     def __init__(self):
-        self.way_point = [[1.0, 0.0, 0], [999, 999, 999]]
+        rospy.init_node('wp_navi', anonymous=True)
+        self.way_point = [999, 999, 999]
+        self.listner = tf.TransformListener()
         self.ac = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self.goal = MoveBaseGoal()
         rospy.on_shutdown(self.shutdown)
@@ -26,38 +29,50 @@ class WpNavi():
             rospy.loginfo("Waiting for the move_base action server to come up.")
         rospy.loginfo("The server comes up.")
 
-        i = 0
-        while not rospy.is_shutdown():
-            target = rospy.wait_for_message("markerXYZRPY", Float32MultiArray)
-            x_ar, z_ar, pitch = target.data[0], target.data[2], target.data[4]
-            
-            self.goal.target_pose.header.frame_id = 't265_odom_frame'
-            self.goal.target_pose.header.stamp = rospy.Time.now()
-            self.goal.target_pose.pose.position.x = -z_ar
-            self.goal.target_pose.pose.position.y = -x_ar
+        target = rospy.wait_for_message("markerXYZRPY", Float32MultiArray)
+        rot_rad_list = [radians(float(angle)) for angle in target.data[3:6]]
+        quat = tf.transformations.quaternion_from_euler(rot_rad_list[0], rot_rad_list[1], rot_rad_list[2])
+        
+        aruco_pose = PoseStamped()
+        aruco_pose.header.frame_id = "d400_color_optical_frame"
+        aruco_pose.pose.position.x = target.data[0]
+        aruco_pose.pose.position.y = target.data[1]
+        aruco_pose.pose.position.z = target.data[2]
+        aruco_pose.pose.orientation.x = quat[0]
+        aruco_pose.pose.orientation.y = quat[1]
+        aruco_pose.pose.orientation.z = quat[2]
+        aruco_pose.pose.orientation.w = quat[3]
 
-            if self.way_point[i][0] == 999:
-                break
-            
+        aruco_transformed = self.listner.transformPose("t265_odom_frame", aruco_pose)
+        self.way_point[0] = aruco_transformed.pose.position.x
+        self.way_point[1] = aruco_transformed.pose.position.y
+        self.way_point[2] = 0.0
 
-            q = tf.transformations.quaternion_from_euler(0, 0, self.way_point[i][2])
+
+        self.goal.target_pose.header.frame_id = 't265_odom_frame'
+        self.goal.target_pose.header.stamp = rospy.Time.now()
+        self.goal.target_pose.pose.position.x = self.way_point[0]
+        self.goal.target_pose.pose.position.y = self.way_point[1]
+
+        if self.way_point[0] != 999:
+            q = tf.transformations.quaternion_from_euler(0, 0, self.way_point[2])
             self.goal.target_pose.pose.orientation = Quaternion(q[0],q[1],q[2],q[3])
-            rospy.loginfo("Sending goal: No" + str(i+1))
+            rospy.loginfo("Sending goal")
             self.ac.send_goal(self.goal)
 
-            succeeded = self.ac.wait_for_result(rospy.Duration(30))
-            state = self.ac.get_state()
-            if succeeded:
-                rospy.loginfo("Succeeded: No."+str(i+1)+"("+str(state)+")")
-            else:
-                rospy.loginfo("Failed: No."+str(i+1)+"("+str(state)+")")
+            rospy.loginfo(aruco_pose)
+            rospy.loginfo(aruco_transformed)
 
-            i = i + 1
+        succeeded = self.ac.wait_for_result(rospy.Duration(30))
+        state = self.ac.get_state()
+        if succeeded:
+            rospy.loginfo("Succeeded")
+        else:
+            rospy.loginfo("Failed")
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('wp_navi', anonymous=True)
-        wp_navi = WpNavi()
+        wp_navi = ArucoNavi()
         wp_navi.process()
         rospy.spin()
     except rospy.ROSInterruptException:
--- onga_perception/scripts/marker_realsense.py
+++ onga_perception/scripts/marker_realsense.py
@@ -1,52 +1,25 @@
 #!/usr/bin/env python3
 
-
 import numpy as np
-import os
 import math
-import time
 import cv2
-
-import struct
-# from cv_bridge import CvBridge
-# bridge = CvBridge()
-
 import rospy
 import ros_numpy
 from std_msgs.msg import Header,String
-from sensor_msgs import point_cloud2
 from sensor_msgs.msg import CameraInfo, Image, PointCloud2, PointField, Image
 from std_msgs.msg import Bool,Float32MultiArray
 from visualization_msgs.msg import Marker,MarkerArray
-
-import glob
 from geometry_msgs.msg import Point,Pose,PoseStamped,PoseArray
-import ctypes
-
-# import open3d as o3d
-
-# import matplotlib.pyplot as plt
-
 import cv2.aruco as aruco
-
-
 
 class DepthImageHandler(object):
     def __init__(self):
         self.CAMINFO = {'topic': '/d400/color/camera_info', 'msg': CameraInfo}
         self.isCamInfo = False
-
         self.COLOR = {'topic': '/d400/color/image_raw', 'msg': Image}
         self.DEPTH = {'topic': '/d400/aligned_depth_to_color/image_raw', 'msg': Image}
         self.PC = {'topic': '/d400/depth/color/points', 'msg': PointCloud2}
 
-        # self.CAMINFO = {'topic': '/realsense/color/camera_info', 'msg': CameraInfo}
-        # self.isCamInfo = False
-
-        # self.COLOR = {'topic': '/realsense/color/image_raw', 'msg': Image}
-        # self.DEPTH = {'topic': '/realsense/depth/image_rect_raw', 'msg': Image}
-        # self.PC = {'topic': '/camera/depth/color/points', 'msg': PointCloud2}
-        
         self.H = 480#720
         self.W = 640#1280
         self.cropSize = 4
@@ -61,12 +34,8 @@ class DepthImageHandler(object):
         self.mask           = np.empty((self.H, self.W), dtype=np.bool)
         self.mask_image     = np.empty((self.H, self.W), dtype=np.uint8)
         self.mask_depth     = np.empty((self.H, self.W), dtype=np.uint8)
-        # self.cropPointCloud = o3d.geometry.PointCloud() 
-
         self.camera_matrix = np.array([[0.0, 0, 0.0], [0, 0.0, 0.0], [0, 0, 1]], dtype=np.float32) 
         self.camera_distortion = np.array([0,0,0,0,0], dtype=np.float32)
-
-
 
     def camInfoCallback(self, msg):
         self.header = msg.header
@@ -79,12 +48,8 @@ class DepthImageHandler(object):
         self.fy = msg.K[4] 
         self.model = msg.distortion_model
         self.isCamInfo = True
-
         self.camera_matrix = np.array([[self.fx, 0, self.ppx], [0, self.fy, self.ppy], [0, 0, 1]], dtype=np.float32) 
         self.camera_distortion = np.array([0,0,0,0,0], dtype=np.float32)
-
-        
-
 
     def colorCallback(self, msg):
         self.color_image = ros_numpy.numpify(msg)
@@ -92,7 +57,6 @@ class DepthImageHandler(object):
 
     def depthCallback(self, msg):
         self.depth_image = ros_numpy.numpify(msg).astype(np.uint16)
-    
 
         # Checks if a matrix is a valid rotation matrix.
     def isRotationMatrix(self,R):
@@ -105,11 +69,8 @@ class DepthImageHandler(object):
     # Calculates rotation matrix to euler angles
     def rotationMatrixToEulerAngles(self,R):
         assert (self.isRotationMatrix(R))
-
         sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
-
         singular = sy < 1e-6
-
         if not singular:
             x = math.atan2(R[2, 1], R[2, 2])
             y = math.atan2(-R[2, 0], sy)
@@ -118,20 +79,14 @@ class DepthImageHandler(object):
             x = math.atan2(-R[1, 2], R[1, 1])
             y = math.atan2(-R[2, 0], sy)
             z = 0
-
         return np.array([x, y, z])
 
     def poseCalculate(self,rvec, tvec):
         rotation_flip = np.array([[1, 0, 0],[0, 1, 0],[0, 0, 1]], dtype=np.int8)
         bufferMatrix = np.array([[0,0,0,1]])
-
         ## marker position relative to camera frame
         rotation_cameratomarker = np.matrix(cv2.Rodrigues(rvec)[0])
         rotation_markertocamera = rotation_cameratomarker.T
-
-        #-- Get the orientation in terms of euler 321 (Needs to be flipped first)
-        # rotationMatrix_marker = rotation_flip*rotation_markertocamera
-        # roll_marker, pitch_marker, yaw_marker = self.rotationMatrixToEulerAngles(rotationMatrix_marker)
 
         #-- Get the orientation in terms of euler 321 (Needs to be flipped first)
         rotationMatrix_camera = rotation_flip*rotation_cameratomarker
@@ -140,28 +95,16 @@ class DepthImageHandler(object):
         xyz_camera = np.array(tvec, dtype=np.float32)/1000
         rpy_camera = np.array([math.degrees(roll_marker), math.degrees(pitch_marker), math.degrees(yaw_marker)], dtype=np.float32)
         marker_pos = np.concatenate((xyz_camera, rpy_camera), axis=0)
-        # ht_marker = np.concatenate((rotationMatrix_marker,np.array([xyz_marker]).T ) , axis=1)
-        # ht_marker = np.concatenate((ht_marker , bufferMatrix), axis=0)
-
-        ## camera position relative to marker
-        # xyz_camera = np.array(-rotation_markertocamera*np.matrix(tvec).T, dtype=np.float32)
-        # xyz_camera = xyz_camera.reshape(3,)
-        # roll_camera, pitch_camera, yaw_camera = self.rotationMatrixToEulerAngles(rotation_flip*rotation_markertocamera)
-        # rpy_camera = np.array([math.degrees(roll_camera), math.degrees(pitch_camera), math.degrees(yaw_camera)], dtype=np.float32)
-        # camera_pos = np.concatenate((xyz_camera, rpy_camera), axis=0)
-
         return marker_pos#, camera_pos, ht_marker
 
     def getMarkerPose(self,img, box, marker_size, camera_matrix, camera_distortion, drawID=True):
         ret = aruco.estimatePoseSingleMarkers(box, marker_size, camera_matrix, camera_distortion)
         #-- Unpack the output, get only the first
         rvec, tvec = ret[0][0,0,:], ret[1][0,0,:] # rotation and translation vector
-
         # aruco.drawDetectedMarkers(img, boxs)
         aruco.drawAxis(img, camera_matrix, camera_distortion, rvec, tvec, 10)
         #marker_pos, camera_pos, ht_marker = self.poseCalculate(rvec, tvec)
         marker_pos = self.poseCalculate(rvec, tvec)
-
         if drawID:
             marker_pos_text = "Marker position x=%4.4f y=%4.4f z=%4.4f roll=%4.4f pitch=%4.4f yaw=%4.4f"%(marker_pos[0], marker_pos[1], marker_pos[2], marker_pos[3], marker_pos[4], marker_pos[5])
             cv2.putText(img, marker_pos_text, (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,255), 2)
@@ -173,10 +116,8 @@ class DepthImageHandler(object):
         imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
         boxs, ids, rejected = aruco.detectMarkers(imgGray, arucoDict, parameters=arucoParam, 
                                                 cameraMatrix=camera_matrix, distCoeff=camera_distortion)
-        
         if draw:
             aruco.drawDetectedMarkers(img, boxs)
-
         return [boxs, ids]    
 
     def boardCastMarkerFromCameraTF(self,markerPose):
@@ -185,20 +126,11 @@ class DepthImageHandler(object):
         self.markerXYZRPY_pub.publish(markerMessage)
         print(markerMessage)
 
-    
-
-        
-    
     def GetMarker(self):
-
         color_image, depth_image    = self.color_image, np.asanyarray(self.depth_image)
         color_image = cv2.cvtColor(color_image, cv2.COLOR_BGRA2BGR)
-        #print(color_image.shape)
         depthForCloud = depth_image
-        # ================================================================================ CHANGED ============
-        # converted to 16bit image
         depth_image = depth_image.astype(np.uint16)
-
         markerID  = 33
         marker_size  = 150
         # markerID  = 5
@@ -217,12 +149,8 @@ class DepthImageHandler(object):
                 if int(id) == markerID:
                     markerPoseFromCam = self.getMarkerPose(color_image, box, marker_size, self.camera_matrix, self.camera_distortion)
                     self.boardCastMarkerFromCameraTF(markerPoseFromCam)
-        
         cv2.imshow("image", color_image)
         cv2.waitKey(1)
-
-
-
 
     def process(self):
         rospy.init_node('pointcloud_masking', anonymous=True)
@@ -231,20 +159,16 @@ class DepthImageHandler(object):
         rospy.Subscriber(self.COLOR['topic'], self.COLOR['msg'], self.colorCallback)
         rospy.Subscriber(self.DEPTH['topic'], self.DEPTH['msg'], self.depthCallback)
         #rospy.Subscriber(self.PC['topic'],      self.PC['msg'],         self.pcCallback)
-        
         #self.depth_scale    = 0.0010000000474974513
-
         ###publisher
         self.markerXYZRPY_pub = rospy.Publisher("markerXYZRPY", Float32MultiArray, queue_size=50)
-
-
         while not rospy.is_shutdown():
                 self.GetMarker()
          
+
 if __name__ == '__main__':
     try:
         _depthImageHandler = DepthImageHandler()
         _depthImageHandler.process()
-
     except rospy.ROSInterruptException:
         pass
