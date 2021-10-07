8ce08e29851ab131e64beead9a4a855d48d295ac
tomokawa <tomokawa.takumi163@mail.kyutech.jp>
2021-10-07 18:53:04 +0900

    fix move_base param

--- onga_control/scripts/twist2pwm_osoyoo.py
+++ onga_control/scripts/twist2pwm_osoyoo.py
@@ -15,7 +15,7 @@ class Twist2duty():
         self.pwm_left = Int64()
         self.pwm_right = Int64()
         self.received_twist = None
-        self.min_duty = 0.15
+        self.min_duty = 20
         rospy.Subscriber('/onga_velocity_controller/cmd_vel', Twist, self.callback)
         self.pub_rpm_right = rospy.Publisher('/right_motor/rpm', Int64, queue_size=10)
         self.pub_duty_right = rospy.Publisher('/right_motor/duty', Int64, queue_size=10)
@@ -39,8 +39,8 @@ class Twist2duty():
         axle_length = 0.1#axle_size(2d)
         gear_rate = 1.0
 
-        v = 2*data.linear.x#(m/s)
-        omega = 2*data.angular.z#(rad/s)
+        v = data.linear.x#(m/s)
+        omega = -3*data.angular.z#(rad/s)
         v_r = (omega*axle_length + 2*v)/2
         v_l = (omega*axle_length - 2*v)/(-2)
 
@@ -59,7 +59,7 @@ class Twist2duty():
         min_rpm = max_rpm*self.min_duty/100
         min_duty = self.min_duty
         # rpm_rate = (max_rpm-min_rpm)/(max_duty-min_duty)#1%に対するrpm
-        rpm_rate = (max_rpm-min_rpm)/100
+        rpm_rate = (float(max_rpm-min_rpm))/100
         
         ##right duty
         if r_rpm > 0:
